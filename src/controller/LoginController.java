5c324f086a277c2df162598bc887820a2e664eca
Sergio <sergiomoramirete@gmail.com>
2025-03-31 09:42:53 +0200

    edicion de login

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import javax.swing.JOptionPane;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Button;
@@ -44,20 +47,6 @@ public class LoginController {
 
     @FXML
     private PasswordField labelPass;
-    
-    void comprobarLogin(LoginController event) {
-    	if(TxtUsuario.getText().isEmpty()) {
-    		labelError.setText("Error: Insertar nombre usuario");
-    		labelError.setVisible(true);
-    		//JOptionPane.showMessageDialog(null, "Error: escribiendo nombre de usuario"); //" 2 opcion 
-    	} else if (TxtContraseña.getText().isEmpty()) {
-    		labelError.setText("Error: Insertar contraseña");
-    		labelError.setVisible(true);
-    	}else {
-    		String usuario =  TxtUsuario.getText();
-    		String contraseña = TxtContraseña.getText();
-    	}
-    }
 
     public void setStage(Stage primaryStage) {
     	stage = primaryStage;
