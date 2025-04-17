71170efce3ec7d2c61e2bee7364639b9865a85c2
zeuslpzz <zeuslopezprior@gmail.com>
2025-04-17 12:12:56 +0200

    Actualizacion de la vida
    Cambio de color de la vida en en equipo pokemón además de que el texto va a juego de la progres bar

--- src/controller/EquipoController.java
+++ src/controller/EquipoController.java
@@ -3,7 +3,9 @@ package controller;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 
+import javafx.scene.control.Label;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -24,21 +26,84 @@ public class EquipoController {
 		this.stage = stage;
 		this.entrenador = ent;
 
+		inicializarVidas();
 	}
 
-	@FXML
-	private Button btnCambiar;
+	 @FXML
+	    private ProgressBar barraVida1;
 
-	@FXML
-	private Button btnSalir;
+	    @FXML
+	    private ProgressBar barraVida2;
 
-	@FXML
-	private ImageView imgfondo;
+	    @FXML
+	    private ProgressBar barraVida3;
+
+	    @FXML
+	    private ProgressBar barraVida4;
+
+	    @FXML
+	    private ProgressBar barraVida5;
+
+	    @FXML
+	    private ProgressBar barraVida6;
+
+	    @FXML
+	    private Button btnCambiar;
+
+	    @FXML
+	    private Button btnSalir;
+
+	    @FXML
+	    private ImageView imgfondo;
+
+	    @FXML
+	    private Label lblVida1;
+
+	    @FXML
+	    private Label lblVida2;
+
+	    @FXML
+	    private Label lblVida3;
+
+	    @FXML
+	    private Label lblVida4;
+
+	    @FXML
+	    private Label lblVida5;
+
+	    @FXML
+	    private Label lblVida6;
 
 	@FXML
 	void cerrarEquipo(ActionEvent event) {
 		menuController.show();
 		this.stage.close();
+	}
+	
+	public void inicializarVidas() {
+		actualizarBarraVida(barraVida1, lblVida1, 213, 218);
+		actualizarBarraVida(barraVida2, lblVida2, 150, 218);
+		actualizarBarraVida(barraVida3, lblVida3, 90, 218);
+		actualizarBarraVida(barraVida4, lblVida4, 40, 218);
+		actualizarBarraVida(barraVida5, lblVida5, 10, 218);
+		actualizarBarraVida(barraVida6, lblVida6, 5, 218);
+	}
+
+	public void actualizarBarraVida(ProgressBar barra, Label label, double vidaActual, double vidaMaxima) {
+	    double porcentaje = vidaActual / vidaMaxima;
+	    barra.setProgress(porcentaje);
+
+	    String color;
+	    if (porcentaje > 0.5) {
+	        color = "#228B22"; // Verde
+	    } else if (porcentaje > 0.2) {
+	        color = "yellow"; // Amarillo
+	    } else {
+	        color = "red"; // Rojo
+	    }
+
+	    barra.setStyle("-fx-accent: " + color + ";");
+	    label.setText((int)vidaActual + "/" + (int)vidaMaxima);
 	}
 
 	@FXML
